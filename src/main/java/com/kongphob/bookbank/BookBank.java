9d034d787d5878ecaa90301fbf2618020982eeb1
Kongphob Nutcharoenkul <67160084@go.buu.ac.th>
2025-09-03 13:46:28 +0700

    Intitail commit

---
+++ src/main/java/com/kongphob/bookbank/BookBank.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.kongphob.bookbank;
+
+/**
+ *
+ * @author User
+ */
+public class BookBank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
